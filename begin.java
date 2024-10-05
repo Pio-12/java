5cf8516e3423fdd6386dab525f3344e03942129f
Pio-12 <arokiyanelsonpio@gmailcom>
2024-10-05 13:49:14 +0530

    intial commit

---
+++ begin.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+class q1{
+    public static void main(String args[])
+    {
+    Scanner scan = new Scanner(System.in);
+    int count = 5;
+   do
+   {
+   System.out.println("enter the num");
+   count =scan.nextInt();
+
+   }while(count<10);
+
+    }
+}
